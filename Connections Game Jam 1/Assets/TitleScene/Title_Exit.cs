f149731f6862c630ae6c6203cefd54faa8ca5ac2
yoshiroh1025 <97860063+yoshiroh1025@users.noreply.github.com>
2022-01-20 04:30:15 +0900

    Add files via upload

--- Connections Game Jam 1/Assets/TitleScene/Title_Exit.cs
+++ Connections Game Jam 1/Assets/TitleScene/Title_Exit.cs
@@ -4,16 +4,83 @@ using UnityEngine;
 
 public class Title_Exit : MonoBehaviour
 {
+
+    SpriteRenderer MainSpriteRenderer;
+    public Sprite Standby;
+    public Sprite Hold;
+    static bool NowSelect;
+    static float value;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        NowSelect = false;
+        value = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        value = Input.GetAxisRaw("Vertical");
+
+        if (value <= -0.1)
+        {
+            ShowUI();
+        }
+        else if (value >= 0.1)
+        {
+            HideUI();
+        }
+
+        if (NowSelect == true && Input.GetButtonDown("Jump") || NowSelect == true && Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            Quit();
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        ShowUI();
+    }
+
+    void OnMouseExit()
+    {
+        HideUI();
+    }
+
+    void OnMouseDown()
+    {
+        if (NowSelect == true)
+        {
+            Quit();
+        }
+    }
+
+    void ShowUI()
+    {
+        if (value >= 0.1)
+        {
+            NowSelect = false;
+        }
+        else
+        {
+            NowSelect = true;
+        }
+        MainSpriteRenderer.sprite = Hold;
+    }
+
+    void HideUI()
+    {
+        if (value <= -0.1)
+        {
+            NowSelect = true;
+        }
+        else
+        {
+            NowSelect = false;
+        }
+        MainSpriteRenderer.sprite = Standby;
     }
 
     void Quit()
@@ -23,10 +90,5 @@ public class Title_Exit : MonoBehaviour
         #elif UNITY_STANDALONE
         UnityEngine.Application.Quit();
         #endif
-    }
-
-    void OnMouseDown()
-    {
-        Quit();
     }
 }
